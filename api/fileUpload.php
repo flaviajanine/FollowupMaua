c1dbdcbff2a22d29b770ee84d7c4021549e4232f
Flávia Janine <flaviabeo@hotmail.com>
2017-10-07 22:44:39 -0300

    Novo background, resultados persistidos no db

--- api/fileUpload.php
+++ api/fileUpload.php
@@ -2,46 +2,34 @@
 
 require_once('config.php');
 
-var_dump($_FILES);
-var_dump($_POST);
-
 ini_set('max_execution_time', 300);
 
 $objDb = new db();
 $link = $objDb->conecta_mysql();
 
-/*
-				$drop = "DROP TABLE IF EXISTS teste" ;
+
+	$drop = "DROP TABLE IF EXISTS teste" ;
 				mysqli_query($link, $drop) or die ("Erro na query drop"); 
 
-				$create = "CREATE TABLE IF NOT EXISTS teste (id int not null primary key auto_increment, RA_HASH varchar(256),P1	FLOAT,P2 FLOAT,PS1 FLOAT,P3	FLOAT,P4 FLOAT,PS2 FLOAT,T1 FLOAT,T2 FLOAT,T3 FLOAT,T4 FLOAT,MT FLOAT,MP FLOAT,MF FLOAT)";
+				$create = $create = "CREATE TABLE IF NOT EXISTS teste(id int primary key auto_increment,Exer int,Esc varchar(45),RA_HASH varchar(256),Cur varchar(45),Ser int,Per varchar(45),Fingr varchar(45),StIni varchar(45),StFin   varchar(45),DtSitCur varchar(45),CdDis varchar(45),CurDis varchar(45),SerDis   int,PerDis varchar(45),G int,T int,L int,OriDisc varchar(45),StIniDisc varchar(45),StFinDisc varchar(45),DtStDisc date,SemDisc varchar(45),CritDisc varchar(45),QtTrabalhos int,KT varchar(45),KP varchar(45),P1 varchar(45),P2 varchar(45),PS1 varchar(45),P3 varchar(45),P4 varchar(45),PS2 varchar(45),T1 varchar(45),T2 varchar(45),T3 varchar(45),T4 varchar(45),T5 varchar(45),T6 varchar(45),T7 varchar(45),T8 varchar(45),T9 varchar(45),T10 varchar(45),T11 varchar(45),T12 varchar(45),T13 varchar(45),T14 varchar(45),T15 varchar(45),T16 varchar(45),MT varchar(45),MP varchar(45),MF varchar(45),Aprovado varchar(45),Acuracia varchar(45))";
 
 				mysqli_query($link, $create) or die ("Erro na query create");
-*/
+
 
     $filename=$_FILES["selectFile"]["tmp_name"];	
-    var_dump($_FILES);	
-
-
+   	
 	if($_FILES["selectFile"]["size"] > 0)
 	{
 				$file = fopen($filename, "r");
-
 				while (($getData = fgetcsv($file, 10000, ";")) !== FALSE){
 
-					$create = "CREATE TABLE IF NOT EXISTS teste(id int primary key auto_increment,Exer int,
-					Esc varchar(45),RA_HASH varchar(256),Cur varchar(45),Ser int,Per varchar(45),Fingr varchar(45),
-					StIni varchar(45),StFin   varchar(45),DtSitCur date,CdDis 
-					varchar(45),CurDis varchar(45),SerDis   int,PerDis varchar(45),G int,T int,L int,OriDisc varchar(45),
-					StIniDisc varchar(45),StFinDisc varchar(45),DtStDisc date,SemDisc varchar(45),CritDisc varchar(45),
-					QtTrabalhos int,KT FLOAT,KP FLOAT,P1 FLOAT,P2 FLOAT,PS1 FLOAT,P3 FLOAT,P4 FLOAT,PS2 FLOAT,T1 FLOAT,
-					T2 FLOAT,T3 FLOAT,T4 FLOAT,T5 FLOAT,T6 FLOAT,T7 FLOAT,T8 FLOAT,T9 FLOAT,T10 FLOAT,T11 FLOAT,T12 FLOAT,
-					T13 FLOAT,T14 FLOAT,T15 FLOAT,T16 FLOAT,MT FLOAT,MP FLOAT,MF FLOAT,Aprovado varchar(45),
-					Acuracia FLOAT)";
-					
-					mysqli_query($link, $create) or die ("Erro na query create");
-		}
+					$sql = "INSERT into teste (Exer,Esc,RA_HASH,Cur,Ser,Per,Fingr,StIni,StFin,DtSitCur,CdDis,CurDis,SerDis,PerDis,G,T,L,OriDisc,StIniDisc,StFinDisc,DtStDisc,SemDisc,CritDisc,QtTrabalhos,KT,KP,P1,P2,PS1,P3,P4,PS2,T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,MT,MP,MF) values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."','".$getData[6]."','".$getData[7]."','".$getData[8]."','".$getData[9]."','".$getData[10]."','".$getData[11]."','".$getData[12]."','".$getData[13]."','".$getData[14]."','".$getData[15]."','".$getData[16]."','".$getData[17]."','".$getData[18]."','".$getData[19]."','".$getData[20]."','".$getData[21]."','".$getData[22]."','".$getData[23]."','".$getData[24]."','".$getData[25]."','".$getData[26]."','".$getData[27]."','".$getData[28]."','".$getData[29]."','".$getData[30]."','".$getData[31]."','".$getData[32]."','".$getData[33]."','".$getData[34]."','".$getData[35]."','".$getData[36]."','".$getData[37]."','".$getData[38]."','".$getData[39]."','".$getData[40]."','".$getData[41]."','".$getData[42]."','".$getData[43]."','".$getData[44]."','".$getData[45]."','".$getData[46]."','".$getData[47]."','".$getData[48]."','".$getData[49]."','".$getData[50]."')";
 
+					mysqli_query($link, $sql) or die ("Erro na query INSERT"); 
+
+
+
+		}
+		
 		fclose($file);	
 	}
-
---
+++ api/post.php
@@ -0,0 +1,85 @@
+<?php 
+
+
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+
+ini_set('max_execution_time', 300);
+
+require_once('config.php');
+
+
+$header = ["Exer", "Esc", "RA_HASH", "Cur", "Ser", "Per", "Fingr", "StIni", "StFin", "DtSitCur", "CdDis", "CurDis", "SerDis", "PerDis", "G", "T", "L", "OriDisc", "StIniDisc", "StFinDisc", "DtStDisc", "SemDisc", "CritDisc", "QtTrabalhos", "KT", "KP", "P1", "P2", "PS1", "P3", "P4", "PS2", "T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", "T11", "T12", "T13", "T14", "T15", "T16", "MT", "MP", "MF"];
+
+
+//PEGAR DINAMICAMENTE O NOME DO MODELO.
+$tablename = "Treino1.xls";
+
+$objDb = new db();
+$link = $objDb->conecta_mysql();
+
+$sql = "SELECT * FROM teste";
+
+$res = mysqli_query($link, $sql) or die ("Erro na query select");
+
+if (mysqli_num_rows($res) > 0){
+
+	while ($linha = mysqli_fetch_assoc($res)) {
+		
+		$data[] = array($linha['Exer'],$linha['Esc'],$linha['RA_HASH'],$linha['Cur'],$linha['Ser'],$linha['Per'],$linha['Fingr'],$linha['StIni'],$linha['StFin'],"01-02-17",$linha['CdDis'],$linha['CurDis'],$linha['SerDis'],$linha['PerDis'],$linha['G'],$linha['T'],$linha['L'],$linha['OriDisc'],$linha['StIniDisc'],$linha['StFinDisc'],$linha['DtStDisc'],$linha['SemDisc'],$linha['CritDisc'],$linha['QtTrabalhos'],$linha['KT'],$linha['KP'],$linha['P1'],$linha['P2'],$linha['PS1'],$linha['P3'],$linha['P4'],$linha['PS2'],$linha['T1'],$linha['T2'],$linha['T3'],$linha['T4'],$linha['T5'],$linha['T6'],$linha['T7'],$linha['T8'],$linha['T9'],$linha['T10'],$linha['T11'],$linha['T12'],$linha['T13'],$linha['T14'],$linha['T15'],$linha['T16'],$linha['MT'],$linha['MP'],$linha['MF']);
+
+	}
+
+}
+
+$params = array("tablename"=>$tablename, "header"=>$header, "data"=>$data);
+
+$body = json_encode($params);
+
+// Setup cURL
+	$ch = curl_init('https://ibm-watson-ml.mybluemix.net/pm/v1/score/P3?accesskey=5yXyFZuLy7uXMxMqFZsjXMG2lnvy48RqATPaknXgwstjpDer/bvOG9LshGMltn4uHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi+H0ZOFm8wNYEEAISjNZ5ulwaR1+ZBm1y9+T/UDJtYvbXhzUtXufnZwJ+gQBSAR2uqE=');
+	curl_setopt_array($ch, array(
+    CURLOPT_POST => TRUE,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json'
+    ),
+    CURLOPT_POSTFIELDS => $body
+));
+
+// Send the request
+$response = curl_exec($ch);
+//echo $response;
+
+
+// Check for errors
+if($response === FALSE){
+    die(curl_error($ch));
+}
+
+// Decode the response
+$responseData = json_decode($response, TRUE);
+
+//print_r($responseData);
+
+#	GUARDAR AS RESPOSTAS NO BANCO DE DADOS (INSERT GERAL);
+
+$x = mysqli_num_rows($res);
+
+
+for ($i=2; $i <= $x; $i++){ 
+
+
+	$var1 = $responseData[0]['data'][$i-1][51];
+	$var2 = $responseData[0]['data'][$i-1][52];
+
+
+	$sql = "UPDATE teste SET Aprovado = '$var1', Acuracia = '$var2' WHERE id = '$i'";
+
+	$r = mysqli_query($link, $sql) or die ("Erro na query insert");
+
+
+}
+
+
+
